7e77516161ba5f6e4716c4c1c39aeeab0150ab39
Atahan Tuncer <atahan@Atahans-MacBook-Pro.local>
2022-03-10 04:07:01 +0300

    init and Devise configured

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  enum role: [:user, :owner]
+  after_initialize :set_default_role, :if => :new_record?
+  def set_default_role
+    self.role ||= :user
+  end
+end
