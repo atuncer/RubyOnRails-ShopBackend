5674624b892c0169364e43d852fb0d1ecdcf06a2
Atahan Tuncer <atahan@Atahans-MacBook-Pro.local>
2022-03-12 06:17:16 +0300

    tests for ShopController as Other Shop Owner and Non Shop Owner is completed. TODO: fix failed tests

--- test/controllers/shops_controller_test.rb
+++ test/controllers/shops_controller_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class ShopsControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
@@ -11,51 +11,162 @@ class ShopsControllerTest < ActionDispatch::IntegrationTest
   end
 
   # SHOP OWNER
-  test "should get index as SHOP OWNER" do
+  test 'should get index as SHOP OWNER' do
     sign_in @shop_owner
     get shops_url
     assert_response :success
   end
 
-  test "should get new as SHOP OWNER" do
+  test 'should NOT get new as SHOP OWNER' do
     sign_in @shop_owner
     get new_shop_url
-    assert_response :success
+    assert_response :error
   end
 
-  test "should NOT create shop as SHOP OWNER" do
+  test 'should NOT create shop as SHOP OWNER' do
     sign_in @shop_owner
-    old_shop_count = Shop.count
-    post shops_url, params: { shop: { "name"=>"Test Name", "bio"=>"Test Bio" } }
-    assert_equal old_shop_count, Shop.count
+    assert_no_difference('Shop.count') do
+      post shops_url, params: { shop: { 'name' => 'Test Name', 'bio' => 'Test Bio' } }
+    end
 
     assert_response :unprocessable_entity
   end
 
-  test "should show shop as SHOP OWNER" do
+  test 'should show shop as SHOP OWNER' do
     sign_in @shop_owner
     get shop_url(@shop)
     assert_response :success
   end
 
-  test "should get edit as SHOP OWNER" do
+  test 'should get edit as SHOP OWNER' do
     sign_in @shop_owner
     get edit_shop_url(@shop)
     assert_response :success
   end
 
-  test "should update shop as SHOP OWNER" do
+  test 'should update shop as SHOP OWNER' do
     sign_in @shop_owner
-    patch shop_url(@shop), params: { shop: { "name"=>"Test Name", "bio"=>"Test Bio" } }
+    assert_changes -> { @shop.reload.name }, -> { @shop.reload.bio } do
+      patch shop_url(@shop), params: { shop: { 'name' => 'Updated Name', 'bio' => 'Updated Bio' } }
+    end
     assert_redirected_to shop_url(@shop)
   end
 
-  test "should destroy shop as SHOP OWNER" do
+  test 'should destroy shop as SHOP OWNER' do
     sign_in @shop_owner
-    assert_difference("Shop.count", -1) do
+    assert_difference('Shop.count', -1) do
       delete shop_url(@shop)
     end
 
     assert_redirected_to shops_url
   end
+
+  # OTHER SHOP OWNER
+
+  test 'should get index as OTHER SHOP OWNER' do
+    sign_in @other_shop_owner
+    get shops_url
+    assert_response :success
+  end
+
+  test 'should NOT get new as OTHER SHOP OWNER' do
+    sign_in @other_shop_owner
+    get new_shop_url
+    assert_response :error
+  end
+
+  test 'should NOT create shop as OTHER SHOP OWNER' do
+    sign_in @other_shop_owner
+    assert_no_difference('Shop.count') do
+      post shops_url, params: { shop: { 'name' => 'Test Name', 'bio' => 'Test Bio' } }
+    end
+
+    assert_response :unprocessable_entity
+  end
+
+  test 'should show shop as OTHER SHOP OWNER' do
+    sign_in @other_shop_owner
+    get shop_url(@shop)
+    assert_response :success
+  end
+
+  test 'should NOT get edit as OTHER SHOP OWNER' do
+    sign_in @other_shop_owner
+    get edit_shop_url(@shop)
+    assert_redirected_to shops_url
+  end
+
+  test 'should NOT update shop as OTHER SHOP OWNER' do
+    sign_in @other_shop_owner
+    assert_no_changes -> { @shop.reload.name }, -> { @shop.reload.bio } do
+      patch shop_url(@shop), params: { shop: { 'name' => 'Updated Name', 'bio' => 'Updated Bio' } }
+    end
+    assert_redirected_to shops_url
+  end
+
+  test 'should NOT destroy shop as OTHER SHOP OWNER' do
+    sign_in @other_shop_owner
+    assert_no_difference('Shop.count') do
+      delete shop_url(@shop)
+    end
+
+    assert_redirected_to shops_url
+  end
+
+  # NON SHOP OWNER (has access to creating a new shop)
+  test 'should get index as NON SHOP OWNER' do
+    sign_in @non_shop_owner
+    get shops_url
+    assert_response :success
+  end
+
+  test 'should get new as NON SHOP OWNER' do
+    sign_in @non_shop_owner
+    get new_shop_url
+    assert_response :success
+  end
+
+  test 'should create shop as NON SHOP OWNER' do
+    sign_in @non_shop_owner
+    assert_difference('Shop.count') do
+      post shops_url, params: { shop: { 'name' => 'Test Name', 'bio' => 'Test Bio' } }
+    end
+
+    assert_redirected_to shop_url(Shop.last)
+  end
+
+  test 'should show shop as NON SHOP OWNER' do
+    sign_in @non_shop_owner
+    get shop_url(@shop)
+    assert_response :success
+  end
+
+  test 'should NOT get edit as NON SHOP OWNER' do
+    sign_in @non_shop_owner
+    get edit_shop_url(@shop)
+    assert_redirected_to shops_url
+  end
+
+  test 'should NOT update shop as NON SHOP OWNER' do
+    sign_in @non_shop_owner
+
+    assert_no_changes -> { @shop.reload.name }, -> { @shop.reload.bio } do
+      patch shop_url(@shop), params: { shop: { 'name' => 'Updated Name', 'bio' => 'Updated Bio' } }
+    end
+    assert_redirected_to shops_url
+  end
+
+  test 'should NOT destroy shop as NON SHOP OWNER' do
+    sign_in @non_shop_owner
+    assert_no_difference('Shop.count') do
+      delete shop_url(@shop)
+    end
+
+    assert_redirected_to shops_url
+  end
+
+  # NON LOGGED IN USER
+  # TODO: test for non logged in user
+  # PLAIN USER (that has no access creating a new shop)
+  # TODO: test for plain user
 end
