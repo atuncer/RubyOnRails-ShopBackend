7464565d74081faaf54dc0791bad81fa58de0d8f
Onurcan <mehmet.yuksel@ozu.edu.tr>
2022-05-30 21:09:27 +0300

    yessirskie

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source "https://rubygems.org"
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby "3.0.0"
+ruby "3.1.2"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.2", ">= 7.0.2.3"
@@ -10,11 +10,14 @@ gem "rails", "~> 7.0.2", ">= 7.0.2.3"
 gem "sprockets-rails"
 
 # Use postgres as the database for Active Record
-gem "pg", "~> 1.3.4"
+# gem "pg", "~> 1.3.4"
 
+gem "sqlite3"
 gem "rename"
 
 gem "devise", "~> 4.8"
+
+gem 'tzinfo-data'
 
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", "~> 5.0"
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_05_25_181741) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "favorite_items", force: :cascade do |t|
     t.integer "item_id"
     t.integer "user_id"
@@ -34,12 +31,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_25_181741) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "shop_id", null: false
+    t.integer "shop_id", null: false
     t.index ["shop_id"], name: "index_items_on_shop_id"
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.bigint "item_id", null: false
+    t.integer "item_id", null: false
     t.string "comment"
     t.float "star"
     t.datetime "created_at", null: false
@@ -52,7 +49,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_25_181741) do
     t.string "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
+    t.integer "user_id", null: false
     t.index ["user_id"], name: "index_shops_on_user_id", unique: true
   end
 
